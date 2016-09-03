509b7704ad03b09644856d0177cb049298e0c8d9
peacekeeper <markus@projectdanube.org>
2016-09-03 15:16:42 +0300

    WIP

--- src/main/webapp/hex2base.js
+++ src/main/webapp/hex2base.js
@@ -43,3 +43,19 @@ function hexToPem(s) {
    var pem = b.match(/.{1,64}/g).join("\n");
    return "-----BEGIN CERTIFICATE-----\n" + pem + "\n-----END CERTIFICATE-----"; 
 }
+
+function convertFromHex(hex) {
+    var hex = hex.toString();//force conversion
+    var str = '';
+    for (var i = 0; i < hex.length; i += 2)
+        str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+    return str;
+}
+
+function convertToHex(str) {
+    var hex = '';
+    for(var i=0;i<str.length;i++) {
+        hex += ''+str.charCodeAt(i).toString(16);
+    }
+    return hex;
+}
